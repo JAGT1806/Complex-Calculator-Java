1f7d9265a7dbe1c54a139bb4263d894a3c9382ac
JAGT1806 <JAGT1806@gmail.com>
2024-01-02 13:23:00 -0500

    Métodos de impresión de números complejos en binómica y polar

--- ComplexCalculator/src/complexcalculator/ComplexCalculator.java
+++ ComplexCalculator/src/complexcalculator/ComplexCalculator.java
@@ -6,7 +6,13 @@ import javax.swing.JOptionPane; // Import the librarie JOptionPane
  * @author JAGT1806
  */
 public class ComplexCalculator {
-
+    // Parte binómico
+    float real;
+    float imaginario;
+    
+    // Parte polar
+    float modulo;
+    float angulo;
 
     public static void main(String[] args) {
         menu();
@@ -27,9 +33,12 @@ public class ComplexCalculator {
         
         switch(optionMenu){
             case 1 -> {menuBinomic();}
-            case 2 -> {menuPolar()}
+            case 2 -> {menuPolar();}
             case 0 -> System.exit(0);
-            default -> System.out.println("Opción Inválida");
+            default -> {
+                JOptionPane.showMessageDialog(null, "Opción errónea\n");
+                menu();
+            }
         
         }
     }
@@ -70,7 +79,7 @@ public class ComplexCalculator {
                          2. Dividir
                          3. Potencia
                          4. Raíz
-                         5. Bin/u00f3mica
+                         5. Bin\u00f3mica
                          0. Regresar
                                                    
                          OPCIÓN: """));
@@ -90,4 +99,38 @@ public class ComplexCalculator {
         
     }
     
+    // Métodos dinámicos
+    
+      // Muestra el número complejo de forma binómica
+    public String showComplexBinomic() {
+        String show = "";
+        
+        if (this.real != 0) { // Si el valor real no es cero
+            show = show + this.real; // Salida : +-a
+            if (this.imaginario != 0) { // Si el valor imaginario no es cero
+                if (this.imaginario > 0) { // En caso de ser positivo
+                    show = show + " + " + this.imaginario + "i"; // Salida : +-a + bi
+                } else { // En caso de ser negativo
+                    show = show + " - " + Math.abs(this.imaginario) + "i"; // Salida : +- a - bi
+                }
+            } else {
+                // Si vale 0, no imprime nada
+            }
+        } else if (this.imaginario != 0) {
+            show = show + this.imaginario + "i"; // +- bi
+        } else {
+            show = show + "0"; // 0
+        }
+        
+        return show;
+    }
+    
+      // Muestra el número complejo de forma polar
+    public String showComplexPolar() {
+        String show = "";
+        
+        show = show + this.modulo + " cis" + "(" + this.angulo + ") "; // Z : r cis(O)
+        
+        return show;
+    }
 }
